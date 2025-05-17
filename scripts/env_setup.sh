69dea476e7fc4f154de692f5c13d2287138e89dc
xirzo <easttem@gmail.com>
2025-05-17 22:27:38 +0300

    fix: add setfont only if not present

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-printf '\nsetfont ter-132n\n' >>~/.bashrc
+if ! grep -q "^setfont ter-132n$" ~/.bashrc; then
+    printf '\nsetfont ter-132n\n' >>~/.bashrc
+    echo "Font setting added to .bashrc"
+else
+    echo "Font setting already exists in .bashrc, no changes made"
+fi
