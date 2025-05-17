3412a0bf805b9203e309359fff47e9b499f14bc9
xirzo <easttem@gmail.com>
2025-05-17 23:36:58 +0300

    feat: precreate wg network

--- scripts/main.sh
+++ scripts/main.sh
@@ -4,5 +4,7 @@ source ./packages.sh
 source ./systemd_services.sh
 source ./env_setup.sh
 
+docker network create wg_network
+
 bash ./create_docker_app.sh "wireguard" "../docker-compose/docker-compose-wireguard.yml"
 bash ./create_docker_app.sh "portainer" "../docker-compose/docker-compose-portainer.yml"
